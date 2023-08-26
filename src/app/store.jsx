9fcad021fc0ef26c0a9051e617495a62ae4242dc
Achieve-software <vtcbcdtyr@gmail.com>
2023-08-26 15:08:30 +0300

    state

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -11,7 +11,7 @@ const store = configureStore ({
         news:newsReducer,
     },
 
-    devTools:process.env.NODE_ENV !== "development"
+    // devTools:process.env.NODE_ENV !== "development"
     devTools:process.env.NODE_ENV !== "production"
 })
 
